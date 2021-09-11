34aa450c0b1cfec884de6c146cb4c39a98510803
dididnk <ngbamedistingue@yahoo.com>
2021-09-11 21:42:18 +0200

    fin de la partie 2: Private, setter, getter

--- PHP/POO/Exercice1/index.php
+++ PHP/POO/Exercice1/index.php
@@ -1,42 +1,10 @@
 <?php
-require_once("Classes/Comptes.php");
+require_once("Classes/Compte.php");
 
 // on instancie la classe
-$compte1 = new Comptes("Emmanuel", 300);
+$compte1 = new Compte("Emmanuel", 300);
 
-// on écrit dans la propriété titulaire
-// $compte1->titulaire = 'Christelle';
+echo $compte1->getTitulaire().'<br/>';
 
-// on écrit dans la propriété solde
-// $compte1->solde  = 5000;
-
-?>
-<p><?= $compte1->voirSolde() ?></p>;
-<?php
-
-// on depose 200 euros
-$compte1->deposer(200);
-?>
-<p><?= $compte1->voirSolde() ?></p>;
-<?php
-// on depose un montant invalide
-$compte1->deposer(-50);
-
-?>
-<p><?= $compte1->voirSolde() ?></p>;
-<?php
-// on retire 450
-$compte1->retirer(450);
-
-?>
-<p><?= $compte1->voirSolde() ?></p>;
-<?php
-// on retire un montant invalide
-$compte1->retirer(8000);
-?>
-<p><?= $compte1->voirSolde() ?></p>;
-<?php
-
-$compte2 = new Comptes("Distingué");
-
-var_dump($compte1);
+echo "Le taux d'interêt du compte est : ".compte::TAUX_INTERETS." euros.<br/>";
+// var_dump($compte1);
