08e7a55ef894e5aec65bdbfb9822039ef27cb65b
Emmanuel <ngbamedistingue@yahoo.com>
2021-10-23 18:50:11 +0200

    Exercice sur le tableau

--- JAVA/MainApp.java
+++ JAVA/MainApp.java
@@ -1,7 +1,26 @@
+
 class MainApp
 {
-    public static void main (String[] args)
+    public static void main(String[] args)
     {
-        System.out.println("Bonjour le monde");
+        int[][] tab =
+        {
+            {1, 2, 3},
+            {2, 4, 5}
+        };
+        for(int[] tableau: tab)
+        {
+            for(int element_tableau: tableau)
+                System.out.println(element_tableau);
+        }
+        /*
+        for(int i  = 0; i < tab.length; i++)
+        {
+            for(int j = 0; j < tab[i].length; j++)
+            {
+                System.out.println(tab[i][j]);
+            }
+        }
+        */
     }
 }
