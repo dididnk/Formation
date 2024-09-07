e9785aa7e82a565a63f3822349cb0ed1d8f7ed64
Emmanuel NGBAME <ngbamedistingue@yahoo.com>
2024-09-08 01:25:31 +0200

    add services, aggregate, events, etc.

---
+++ CQRS/Exercice/CQRS-ES/CQRS.Core/Domain/AggregateRoot.cs
@@ -0,0 +1,56 @@
+﻿using CQRS.Core.Events;
+
+namespace CQRS.Core.Domain
+{
+	public class AggregateRoot
+	{
+		public Guid _id;
+		public readonly List<BaseEvent> _changes = new();
+
+		public Guid Id
+		{
+			get { return _id; }
+		}
+		public int Version { get; set; } = -1;
+
+		public IEnumerable<BaseEvent> GetUncommittedChanges()
+		{
+			return _changes;
+		}
+
+		public void MarkChangesAsCommitted()
+		{
+			_changes.Clear();
+		}
+
+		public void ApplyChnages(BaseEvent @event, bool isNew)
+		{
+			var method = this.GetType().GetMethod("Apply", new Type[] { @event.GetType() });
+
+			if (method == null)
+			{
+				throw new ArgumentException(nameof(method), $"The Apply method was not found in aggregate for {@event.GetType().Name} !");
+			}
+
+			method.Invoke(this, new object[] { @event });
+
+			if (isNew)
+			{
+				_changes.Add(@event);
+			}
+		}
+
+		protected void RaiseEvent(BaseEvent @event)
+		{
+			ApplyChnages(@event, true);
+		}
+
+		public void ReplayEvents(IEnumerable<BaseEvent> events)
+		{
+			foreach (var @event in events)
+			{
+				ApplyChnages(@event, false);
+			}
+		}
+	}
+}
